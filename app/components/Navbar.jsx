4450f6d29ce762d3382f0cc1f6d1d59bcbd641bf
Emmanuel Inua <artofnuel@gmail.com>
2023-09-23 12:38:00 +0100

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { MdClose } from "react-icons/md";
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(true);
   return (
     <header className="bg-white text-[#003A8E]">
       <nav className="w-full h-auto   p-4 md:p-6 md:px-10 flex justify-between items-center relative">
@@ -25,7 +25,7 @@ const Navbar = () => {
         </div>
 
         {toggle && (
-          <ul className="md:hidden w-1/2 h-screen bg-white flex flex-col justify-start items-center p-2 gap-5 font-semibold text-lg absolute right-0 top-[70px]">
+          <ul className="md:hidden w-1/2 h-[270px] bg-white flex flex-col justify-start items-center p-2 gap-5 font-semibold text-lg absolute right-0 top-[80px] rounded-md">
             <li className="p-2">About</li>
             <li className="p-2">Training</li>
             <li className="p-2">Services</li>
